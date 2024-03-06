64bb741383e4f9cc52f9d05dc792e8d625c6f892
Yeranosyan <karlenovich@yahoo.com>
2024-03-05 18:26:29 -0800

    build and test a loop for entering new pet data

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c563d514c23a52ca54d8b68da13e4cd4417a2090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8ca5912326ad10860fd6e7f35d68bf788c5808")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
